5f4ddf5bb82167a5c31805cda6a32262a5429d5c
Karina Chen <karina6188@gmail.com>
2019-08-14 10:15:20 -0700

    Modified app.js and HTML

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 var hours = ['6am ', '7am ', '8am ', '9am ', '10am ', '11am ', '12pm ', '1pm ', '2pm ', '3pm ', '4pm ', '5pm ', '6pm ', '7pm ', '8pm '];
 
-var sumTotal = ['Total: '];
+var sumTotal = ['Total '];
 
 // All values in each Object.
 var allStores = [];
